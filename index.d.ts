8bba343cbfdb4d02e3c0cbec998fc83f30d0a4fc
Iuliia Shnai <yul.schnay@gmail.com>
2023-05-08 16:16:48 +0200

    filters and subject

--- index.d.ts
+++ index.d.ts
@@ -28,7 +28,7 @@ interface AnalysisResult {
 interface PostData {
   post: string;
   name: string;
-  class: string;
+  subject: string;
   originalPost: string;
   sentiment: AnalysisResult;
   postMedia: boolean;
