4714e2baad5be87d7984e5f6379012117168d8d8
Iuliia Shnai <yul.schnay@gmail.com>
2024-08-28 16:34:17 +0200

    footer removed

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,7 +2,7 @@
 
 import { useState, useEffect } from "react";
 import Navbar from "@/components/navbar";
-import Footer from "@/components/footer";
+// import Footer from "@/components/footer";
 import { MyChart } from "@/components/ui/charts";
 import {
   Card,
